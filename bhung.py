62434d709ff556d594a5659831b23bd54a69c4f9
PhanPhamBaoHung <baohungphan04062004@gmail.com>
2024-09-28 08:29:55 +0700

    Create bhung.py

---
+++ bhung.py
@@ -0,0 +1,17 @@
+import matplotlib.pyplot as plt
+
+# Dữ liệu
+x = [0, 1, 2, 3, 4, 5]
+y = [0, 1, 4, 9, 16, 25]
+
+# Vẽ biểu đồ
+plt.plot(x, y, marker='o')
+
+# Thêm tiêu đề và nhãn
+plt.title('Biểu đồ hàm số y = x^2')
+plt.xlabel('Giá trị x')
+plt.ylabel('Giá trị y')
+
+# Hiển thị biểu đồ
+plt.grid(True)
+plt.show()
